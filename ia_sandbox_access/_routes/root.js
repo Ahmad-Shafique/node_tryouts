2de7785304f90507f299ff0069d44e1e9ba2f545
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2018-02-20 23:53:38 +0600

    debugged and select query tested

--- ia_sandbox_access/_routes/root.js
+++ ia_sandbox_access/_routes/root.js
@@ -9,6 +9,40 @@ const tdrCreditRatingRoutes = require('./tdrCreditRating');
 const tdrInvestmentRoutes = require('./tdrInvestment');
 
 module.exports = function(app,db){
+
+    app.get('/ia-get-request-test', (req,res) => {
+        console.log('IA get request perfectly healthy.');
+        console.log('Sending greetings now...');
+        res.send("Hello World");
+    });
+    app.post('/ia-post-request-test', (req,res) => {
+        console.log('IA post request perfectly healthy.');
+        console.log('Sending greetings now...');
+        res.send("Hello World");
+    });
+
+    app.get('/test-mysql', (req,res) => {
+        var connection = db.createConnection({
+            host:'localhost',
+            user:'root',
+            database: 'ia'
+        });
+
+        connection.connect(function(err){
+            if(err) throw err;
+            console.log('Connection to MySQL established !!!');
+        });
+
+        let sql = 'select * from user';
+
+        connection.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    });
+
+
+
     userRoutes(app,db);
     depositRateRoutes(app,db);
     instituteRoutes(app,db);
--- ia_sandbox_access/_routes/users.js
+++ ia_sandbox_access/_routes/users.js
@@ -2,26 +2,5 @@
 
 
 module.exports = function(app,db){
-    app.get('/ia-get-request-test', (req,res) => {
-        console.log('IA get request perfectly healthy.');
-        console.log('Sending greetings now...');
-        res.send("Hello World");
-    });
-    app.post('/ia-post-request-test', (req,res) => {
-        console.log('IA post request perfectly healthy.');
-        console.log('Sending greetings now...');
-        res.send("Hello World");
-    });
-
-    app.get('/test-mysql', (req,res) => {
-        var connection = db.createConnection({
-            host:'localhost',
-            user:'root'
-        });
-
-        connection.connect(function(err){
-            if(err) throw err;
-            console.log('Connection to MySQL established !!!');
-        });
-    });
+    
 };
