a299f4f3b050da4903347dbbe306a75715b05a3a
Ahmad Shafique <ahmed-kuwait@hotmail.com>
2018-02-20 23:41:29 +0600

    added necessary controllers

--- ia_sandbox_access/_routes/root.js
+++ ia_sandbox_access/_routes/root.js
@@ -1,27 +1,19 @@
-// const request= require('request');
 
+
+const userRoutes = require('./users');
+const depositRateRoutes = require('./depositRate');
+const instituteRoutes = require('./institute');
+const instituteDirectorRoutes = require('./instituteDirector');
+const newsFeedRoutes = require('./newsFeed');
+const tdrCreditRatingRoutes = require('./tdrCreditRating');
+const tdrInvestmentRoutes = require('./tdrInvestment');
 
 module.exports = function(app,db){
-    app.get('/ia-get-request-test', (req,res) => {
-        console.log('IA get request perfectly healthy.');
-        console.log('Sending greetings now...');
-        res.send("Hello World");
-    });
-    app.post('/ia-post-request-test', (req,res) => {
-        console.log('IA post request perfectly healthy.');
-        console.log('Sending greetings now...');
-        res.send("Hello World");
-    });
-
-    app.get('/test-mysql', (req,res) => {
-        var connection = db.createConnection({
-            host:'localhost',
-            user:'root'
-        });
-
-        connection.connect(function(err){
-            if(err) throw err;
-            console.log('Connection to MySQL established !!!');
-        })
-    })
-};
+    userRoutes(app,db);
+    depositRateRoutes(app,db);
+    instituteRoutes(app,db);
+    instituteDirectorRoutes(app,db);
+    newsFeedRoutes(app,db);
+    tdrCreditRatingRoutes(app,db);
+    tdrInvestmentRoutes(app,db);
+}
